a81863f3a338d6cf1d2bd3286a7791da79550232
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-20 19:50:10 +0300

    refactor: minor refactor

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,8 +1,29 @@
 import * as userService from '../services/user-service.js';
 import * as errorService from '../services/error-service.js';
 
+const signUp = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const foundedUser = await userService.findOneUser({ email });
+    if (foundedUser) {
+      return res.status(409).json({ message: `Conflict: email already exist` });
+    }
+
+    const createdUser = await userService.createUser({ ...req.body, blockedStatus: false });
+    if (!createdUser) {
+      return res.status(400).json({ message: 'Bad request: Something gone wrong...', error });
+    }
+
+    res.status(201).json(createdUser);
+  } catch (error) {
+    errorService.handleError(res, error);
+  }
+};
+
 const signIn = async (req, res) => {
   const { name, password } = req.body;
+
   try {
     const foundedUser = await userService.findOneUser({ name });
 
@@ -20,26 +41,6 @@ const signIn = async (req, res) => {
 
     const updatedUser = await userService.updateUserById(foundedUser._id, { updatedAt: new Date() });
     res.status(200).json(updatedUser);
-  } catch (error) {
-    errorService.handleError(res, error);
-  }
-};
-
-const signUp = async (req, res) => {
-  const { email } = req.body;
-
-  try {
-    const foundedUser = await userService.findOneUser({ email });
-    if (foundedUser) {
-      return res.status(409).json({ message: `Conflict: email already exist` });
-    }
-
-    const createdUser = await userService.createUser({ ...req.body, blockedStatus: false });
-    if (!createdUser) {
-      return res.status(400).json({ message: 'Bad request: Something gone wrong...', error });
-    }
-
-    res.status(201).json(createdUser);
   } catch (error) {
     errorService.handleError(res, error);
   }
