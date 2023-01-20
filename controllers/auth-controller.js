6d5777ce033572d2c3c4701a372b84799a25bc82
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-01-20 19:52:52 +0300

    refactor: minor fix files

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,49 @@
 import * as userService from '../services/user-service.js';
 import * as errorService from '../services/error-service.js';
 
+const signIn = async (req, res) => {
+  const { name, password } = req.body;
+
+  try {
+    const foundedUser = await userService.findOneUser({ name });
+
+    if (!foundedUser) {
+      return res.status(404).json({ message: `Not found: User is not found or User "${name}" have been removed)` });
+    }
+
+    if (foundedUser.blockedStatus) {
+      return res.status(403).json({ message: 'Forbidden: Access is Forbidden' });
+    }
+
+    if (foundedUser.password !== password) {
+      return res.status(401).json({ message: 'Unauthorized: Incorrect password' });
+    }
+
+    const updatedUser = await userService.updateUserById(foundedUser._id, { updatedAt: new Date() });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    errorService.handleError(res, error);
+  }
+};
+
+const signUp = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const foundedUser = await userService.findOneUser({ email });
+    if (foundedUser) {
+      return res.status(409).json({ message: `Conflict: email already exist` });
+    }
+
+    const createdUser = await userService.createUser({ ...req.body, blockedStatus: false });
+    if (!createdUser) {
+      return res.status(400).json({ message: 'Bad request: Something gone wrong...', error });
+    }
+
+    res.status(201).json(createdUser);
+  } catch (error) {
+    errorService.handleError(res, error);
+  }
+};
+
 export { signIn, signUp };
